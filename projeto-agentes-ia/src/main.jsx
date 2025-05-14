20883896a2d2b000e1c8334f9dbac1543237bbd5
Lucas <108193174+lucas-azv@users.noreply.github.com>
2025-05-14 20:22:34 -0300

    importando api do gemini

--- projeto-agentes-ia/src/main.jsx
+++ projeto-agentes-ia/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleGenAI } from "@google/genai";
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const ai = new GoogleGenAI({ apiKey: "YOUR_API_KEY" });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+main();
